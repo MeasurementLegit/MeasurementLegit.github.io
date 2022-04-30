bb865712f77045192fbb2a6a10ae552bf69fe5b4
Geronimo Klokowski <104308966+MeasurementLegit@users.noreply.github.com>
2022-04-30 11:27:44 +0200

    Made title.js more beautiful.

--- js/title.js
+++ js/title.js
@@ -1,54 +1,57 @@
-if(document.addEventListener){
-    document.addEventListener("DOMContentLoaded", function(){
+if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", function() {
         loaded();
     });
-} else if(document.attachEvent){
-    document.attachEvent("onreadystatechange", function(){
+} else if (document.attachEvent) {
+    document.attachEvent("onreadystatechange", function() {
         loaded();
     });
 }
 
-function loaded(){
-    
+function loaded() {
+
     setInterval(loop, 300);
-	
-	var video = document.getElementById('video');
-	video.volume = 0.15;
-	
+
+    var video = document.getElementById('video');
+    video.volume = 0.15;
+
 
 }
+
 function play() {
-	var audio = document.getElementById('video');
-	video.play();
+    var audio = document.getElementById('video');
+    video.play();
 }
 var x = 0;
 
-var titleText = [ "|",
-                    "|",
-                    "M",
-                    "Me",
-                    "Mea",
-                    "Meas",
-                    "Measu",
-                    "Measur",
-                    "Measure",
-                    "Measurem",
-                    "Measureme",
-                    "Measuremen",
-				 	"Measurement",
-				 	"Measurement",
-                    "Measuremen",
-                    "Measureme",
-                    "Measurem",
-                    "Measure",
-                    "Measur",
-                    "Measu",
-                    "Meas",
-					"Mea",
-                    "Me",
-                    "M"];
-function loop(){
+var titleText = ["|",
+    "|",
+    "M",
+    "Me",
+    "Mea",
+    "Meas",
+    "Measu",
+    "Measur",
+    "Measure",
+    "Measurem",
+    "Measureme",
+    "Measuremen",
+    "Measurement",
+    "Measurement",
+    "Measuremen",
+    "Measureme",
+    "Measurem",
+    "Measure",
+    "Measur",
+    "Measu",
+    "Meas",
+    "Mea",
+    "Me",
+    "M"
+];
 
-    document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+function loop() {
+
+    document.getElementsByTagName("title")[0].innerHTML = titleText[x++ % titleText.length];
 
 }
